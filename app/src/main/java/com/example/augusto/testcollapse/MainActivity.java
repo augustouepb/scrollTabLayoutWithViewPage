daf4162296b1ae4bf100202387fb967670f62de7
augustonutes <augusto.bezerra@nutes.uepb.edu.br>
2018-07-26 09:49:56 -0300

    updates

--- app/src/main/java/com/example/augusto/testcollapse/MainActivity.java
+++ app/src/main/java/com/example/augusto/testcollapse/MainActivity.java
@@ -4,39 +4,33 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.design.widget.CollapsingToolbarLayout;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
-    private CollapsingToolbarLayout collapseToolbarLayout;
-    private ViewPager viewPager;
-    private TabLayout tabLayout;
-
-    private DataAdapter dataAdapter;
+    @BindView(R.id.viewpager)  ViewPager viewPager;
+    @BindView(R.id.tabLayout) TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(" ");
         setSupportActionBar(toolbar);
 
-        viewPager = (ViewPager) findViewById(R.id.viewpager);
         FragmentPagerAdapter adapter = new FragmentPageAdapter(getSupportFragmentManager());
 
         viewPager.setAdapter(adapter);
 
-        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(viewPager);
-
-
 
     }
 
